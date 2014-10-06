cd8e9fa35bffaee2481d5ec1ade8ad8775e29fc9
Hackbright Student <info@hackbrightacademy.com>
2014-10-06 16:15:38 -0700

    rewrote logic using dictionaries

--- sorted_data.py
+++ sorted_data.py
@@ -1,17 +1,39 @@
-rest_listing = []
+
 
 filename = open("scores.txt")
 
-for line in filename:
+####################
+# Using an empty list
+
+# rest_listing = []
+
+# for line in filename:
+#     rest_data = line.strip()
+#     rest_listing.append(rest_data)
+
+# rest_listing.sort()
+
+# for pair in rest_listing: 
+#     name, rating = pair.split(":")
+#     print "Restaurant %s is rated at %s" %(name, rating) 
+
+
+###################
+#Using an empty dictionary
+
+rest_dict = {}
+
+for line in filename: 
     rest_data = line.strip()
-    rest_listing.append(rest_data)
+    name, rating = rest_data.split(":")
+    rest_dict[name] = rating
 
-rest_listing.sort()
+key_list = rest_dict.keys()
 
-for pair in rest_listing: 
-    name, rating = pair.split(":")
-    print "Restaurant %s is rated at %s" %(name, rating) 
+key_list.sort()
 
+for key in key_list:
+    print "Restaurant %s is rated at %s" %(key, rest_dict[key]) 
 
 
 
