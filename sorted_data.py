31ba36aa4cca5af36ae634826990a7bfd6f28a05
Hackbright Student <info@hackbrightacademy.com>
2014-10-07 12:18:02 -0700

    Sorted by rating

--- sorted_data.py
+++ sorted_data.py
@@ -22,18 +22,23 @@ filename = open("scores.txt")
 #Using an empty dictionary
 
 rest_dict = {}
+rating_dict = {}
 
 for line in filename: 
     rest_data = line.strip()
     name, rating = rest_data.split(":")
     rest_dict[name] = rating
+    rating_dict.setdefault(rating, []).append(name)
 
-key_list = rest_dict.keys()
+print "Numerically sorted ratings:"
+for rating, restaurants in sorted(rating_dict.items()):
+    for restaurant in sorted(restaurants): # format and alphabetize within rating
+        print "Restaurant %s is rated at %s" % (restaurant, rating) 
 
-key_list.sort()
+print "***********************"
+print "Alphabetized restaurants:"
+for restaurant, rating in sorted(rest_dict.items()):
+    print "Restaurant %s is rated at %s" % (restaurant, rating) 
 
-for key in key_list:
-    print "Restaurant %s is rated at %s" %(key, rest_dict[key]) 
 
-
-
+# filtered_list = filter(lambda x: x % 2 == 0, number_list)
