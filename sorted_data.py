7c8d0345ae05be1cee61d3a6d1e5535423d26994
Hackbright Student <info@hackbrightacademy.com>
2014-10-07 12:59:35 -0700

    Changed lambda to use sorted once on tuple sort.

--- sorted_data.py
+++ sorted_data.py
@@ -27,8 +27,8 @@ rating_dict = {}
 for line in filename: 
     rest_data = line.strip()
     name, rating = rest_data.split(":")
-    rest_dict[name] = rating
-    rating_dict.setdefault(rating, []).append(name)
+    rest_dict[name] = int(rating)
+    rating_dict.setdefault(int(rating), []).append(name)
 
 print "***********************"
 print "Numerically sorted ratings:"
@@ -37,10 +37,10 @@ for rating, restaurants in sorted(rating_dict.items()):
         print "Restaurant %s is rated at %s" % (restaurant, rating) 
 
 print "***********************"
-print "Numerically sorted using 2nd tuple item:"
-# sorts items alphabetically by 1st item (restaurant) in tuple
-# then sorts numerically on the 2nd item (rating) in the tuple
-sorted_tuple_list = sorted(sorted(rest_dict.items()), key=lambda x: x[1])
+print "Numerically sorted highest-lowest using 2nd tuple item:"
+# sorts items numerically first by 2nd tuple item, then alphabetically 
+# using -x[1] tells it to sort by highest to lowest
+sorted_tuple_list = sorted(rest_dict.items(), key=lambda x: (-x[1], x[0]))
 for restaurant, rating in sorted_tuple_list:
     print "Restaurant %s is rated at %s" % (restaurant, rating) 
 
